1f725ed1378c2887027a5c24f148aac0a8c6eaf7
Jalmar01 <villarrealjalmar@hotmail.com>
2023-07-22 13:15:38 -0500

    My first bakend

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,45 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+
+const getAll = catchError(async(req, res) => {
+    const user = await User.findAll()
+    return res.json(user)
+});
+
+const create = catchError(async(req, res)=>{
+    const body = req.body;
+    const user = await User.create(body);
+    return res.status(201).json(user);
+})
+
+const getOne = catchError(async(req, res) => {
+    const {id} = req.params;
+    const user = await User.findByPk(id);
+    if(!user) return res.sendStatus(400);
+    return res.json(user);
+})
+
+const destroy = catchError(async(req, res) =>{
+    const {id} = req.params;
+    const user = await User.destroy({where: {id}})
+    if(!user) return res.sendStatus(400);
+    return res.sendStatus(204);
+});
+
+const update = catchError(async(req, res)=> {
+    const body = req.body;
+    const {id} = req.params;
+    const user = await User.update( body, {
+        where: {id}, returning:true
+    });
+    if(user[0] === 0) return res.sendStatus(400);
+    return res.json(user[1][0]);
+});
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    destroy,
+    update
+};
